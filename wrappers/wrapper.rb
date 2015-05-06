f6207586974fc638cef13b7b05cdfdb13513e2c2
John MacFarlane <jgm@berkeley.edu>
2015-05-06 14:31:04 -0700

    wrapper.rb: added argument for options.

--- wrappers/wrapper.rb
+++ wrappers/wrapper.rb
@@ -4,12 +4,12 @@ require 'ffi'
 module CMark
   extend FFI::Library
   ffi_lib ['libcmark', 'cmark']
-  attach_function :cmark_markdown_to_html, [:string, :int], :string
+  attach_function :cmark_markdown_to_html, [:string, :int, :int], :string
 end
 
 def markdown_to_html(s)
   len = s.bytesize
-  CMark::cmark_markdown_to_html(s, len)
+  CMark::cmark_markdown_to_html(s, len, 0)
 end
 
 STDOUT.write(markdown_to_html(ARGF.read()))
