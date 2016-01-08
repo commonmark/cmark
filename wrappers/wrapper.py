d30371d6871981f369eafb30fb1a7e82df7139eb
John MacFarlane <jgm@berkeley.edu>
2016-01-08 10:03:48 -0800

    Use default opts in python sample wrapper.

--- wrappers/wrapper.py
+++ wrappers/wrapper.py
@@ -22,7 +22,7 @@ markdown = cmark.cmark_markdown_to_html
 markdown.restype = c_char_p
 markdown.argtypes = [c_char_p, c_long, c_long]
 
-opts = 8 # CMARK_OPT_PRETTY
+opts = 0 # defaults
 
 def md2html(text):
     if sys.version_info >= (3,0):
