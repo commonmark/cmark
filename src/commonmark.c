047e65d0b05f601bd0a6fafb4f4eab98659c1529
John MacFarlane <jgm@berkeley.edu>
2016-01-17 21:43:38 -0800

    commonmark: is_autolink - handle case where link has no children.

--- src/commonmark.c
+++ src/commonmark.c
@@ -126,6 +126,9 @@ static bool is_autolink(cmark_node *node) {
   }
 
   link_text = node->first_child;
+  if (link_text == NULL) {
+    return false;
+  }
   cmark_consolidate_text_nodes(link_text);
   realurl = (char *)url->data;
   realurllen = url->len;
