b50b9b5a8c3e2f5d88cbf334e15f4a09c0da307d
Yuki Izumi <kivikakk@github.com>
2016-11-04 12:23:12 +1100

    Add test to illustrate issue

---
+++ test/entity_tests.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+import argparse
+import sys
+import platform
+import html
+from cmark import CMark
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run cmark tests.')
+    parser.add_argument('--program', dest='program', nargs='?', default=None,
+            help='program to test')
+    parser.add_argument('--library-dir', dest='library_dir', nargs='?',
+            default=None, help='directory containing dynamic library')
+    args = parser.parse_args(sys.argv[1:])
+
+cmark = CMark(prog=args.program, library_dir=args.library_dir)
+
+entities5 = html.entities.html5
+entities = sorted([(k[:-1], entities5[k]) for k in entities5.keys() if k[-1] == ';'])
+
+passed = 0
+errored = 0
+failed = 0
+
+print("Testing entities:")
+for entity, utf8 in entities:
+    [rc, actual, err] = cmark.to_html("&{};".format(entity))
+
+    if rc != 0:
+        errored += 1
+        print(entity, '[ERRORED (return code {})]'.format(rc))
+        print(err)
+    elif utf8 in actual:
+        passed += 1
+    else:
+        print(entity, '[FAILED]')
+        print(repr(actual))
+        failed += 1
+
+print("{} passed, {} failed, {} errored".format(passed, failed, errored))
+if (failed == 0 and errored == 0):
+    exit(0)
+else:
+    exit(1)
