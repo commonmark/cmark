657dcc593f9aa1391b71a6a55c6cf9db8185e3fe
Yuki Izumi <kivikakk@github.com>
2016-11-29 15:01:28 +1100

    Strikethrough extension from c068469 reworked
    This is quite straightforward; we do take care in other extensions (i.e.
    autolink) to ensure tildes are left for the strikethrough extension to
    consume.

--- extensions/core-extensions.c
+++ extensions/core-extensions.c
@@ -1,7 +1,10 @@
 #include "core-extensions.h"
+#include "strikethrough.h"
 #include "table.h"
 
 int core_extensions_registration(cmark_plugin *plugin) {
   cmark_plugin_register_syntax_extension(plugin, create_table_extension());
+  cmark_plugin_register_syntax_extension(plugin,
+                                         create_strikethrough_extension());
   return 1;
 }
