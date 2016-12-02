6d5eea30b70d4d58d8f4318b104b15d812215d92
John MacFarlane <jgm@berkeley.edu>
2016-12-02 15:07:10 +0100

    Make sure ENTITYs are recognized as leaf nodes.

--- src/iterator.c
+++ src/iterator.c
@@ -10,7 +10,8 @@ static const int S_leaf_mask =
     (1 << CMARK_NODE_HTML_BLOCK) | (1 << CMARK_NODE_THEMATIC_BREAK) |
     (1 << CMARK_NODE_CODE_BLOCK) | (1 << CMARK_NODE_TEXT) |
     (1 << CMARK_NODE_SOFTBREAK) | (1 << CMARK_NODE_LINEBREAK) |
-    (1 << CMARK_NODE_CODE) | (1 << CMARK_NODE_HTML_INLINE);
+    (1 << CMARK_NODE_CODE) | (1 << CMARK_NODE_HTML_INLINE) |
+    (1 << CMARK_NODE_ENTITY);
 
 cmark_iter *cmark_iter_new(cmark_node *root) {
   if (root == NULL) {
