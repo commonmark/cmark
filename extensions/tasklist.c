4b9523d302aad37e201ead6bbcc1c48a7fcd3016
Ashe Connor <ashe@kivikakk.ee>
2019-02-12 11:19:44 +1100

    remove the class here
    We shouldn't be prescriptive with CSS class names.

--- extensions/tasklist.c
+++ extensions/tasklist.c
@@ -98,7 +98,7 @@ static void html_render(cmark_syntax_extension *extension,
   bool entering = (ev_type == CMARK_EVENT_ENTER);
   if (entering) {
     cmark_html_render_cr(renderer->html);
-    cmark_strbuf_puts(renderer->html, "<li class=\"task-list-item\"");
+    cmark_strbuf_puts(renderer->html, "<li");
     cmark_html_render_sourcepos(node, renderer->html, options);
     cmark_strbuf_putc(renderer->html, '>');
     long userdata = (long)cmark_node_get_user_data(node);
