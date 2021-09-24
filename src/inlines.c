3e77a79aa7b2e3a54f39fb0be191f8b37007096b
Nick Wellnhofer <wellnhofer@aevum.de>
2021-09-24 11:04:17 +0200

    Fix parsing of emphasis before links
    Fix a regression introduced with commit ed0a4bf. Also test the first
    delimiter against stack_bottom in process_emphasis. This happened to
    work with the old code and only resulted in an unnecessary scan.
    
    Fixes #424.

--- src/inlines.c
+++ src/inlines.c
@@ -640,7 +640,8 @@ static cmark_node *handle_period(subject *subj, bool smart) {
 }
 
 static void process_emphasis(subject *subj, bufsize_t stack_bottom) {
-  delimiter *closer = subj->last_delim;
+  delimiter *candidate;
+  delimiter *closer = NULL;
   delimiter *opener;
   delimiter *old_closer;
   bool opener_found;
@@ -650,10 +651,10 @@ static void process_emphasis(subject *subj, bufsize_t stack_bottom) {
                                  stack_bottom, stack_bottom, stack_bottom};
 
   // move back to first relevant delim.
-  while (closer != NULL &&
-         closer->previous != NULL &&
-         closer->previous->position >= stack_bottom) {
-    closer = closer->previous;
+  candidate = subj->last_delim;
+  while (candidate != NULL && candidate->position >= stack_bottom) {
+    closer = candidate;
+    candidate = candidate->previous;
   }
 
   // now move forward, looking for closers, and handling each
