2c62c95124ce00fad20fbbc930f79c62b20b2bb5
Kevin Backhouse <kevinbackhouse@github.com>
2022-11-07 22:29:38 +0000

    Pre-compute number of opening/closing parens to avoid quadratic behavior.

--- extensions/autolink.c
+++ extensions/autolink.c
@@ -35,30 +35,63 @@ static int sd_autolink_issafe(const uint8_t *link, size_t link_len) {
 }
 
 static size_t autolink_delim(uint8_t *data, size_t link_end) {
-  uint8_t cclose, copen;
   size_t i;
+  size_t closing = 0;
+  size_t opening = 0;
 
-  for (i = 0; i < link_end; ++i)
-    if (data[i] == '<') {
+  for (i = 0; i < link_end; ++i) {
+    const uint8_t c = data[i];
+    if (c == '<') {
       link_end = i;
       break;
+    } else if (c == '(') {
+      opening++;
+    } else if (c == ')') {
+      closing++;
     }
+  }
 
   while (link_end > 0) {
-    cclose = data[link_end - 1];
-
-    switch (cclose) {
+    switch (data[link_end - 1]) {
     case ')':
-      copen = '(';
-      break;
-    default:
-      copen = 0;
-    }
-
-    if (strchr("?!.,:*_~'\"", data[link_end - 1]) != NULL)
+      /* Allow any number of matching brackets (as recognised in copen/cclose)
+       * at the end of the URL.  If there is a greater number of closing
+       * brackets than opening ones, we remove one character from the end of
+       * the link.
+       *
+       * Examples (input text => output linked portion):
+       *
+       *        http://www.pokemon.com/Pikachu_(Electric)
+       *                => http://www.pokemon.com/Pikachu_(Electric)
+       *
+       *        http://www.pokemon.com/Pikachu_((Electric)
+       *                => http://www.pokemon.com/Pikachu_((Electric)
+       *
+       *        http://www.pokemon.com/Pikachu_(Electric))
+       *                => http://www.pokemon.com/Pikachu_(Electric)
+       *
+       *        http://www.pokemon.com/Pikachu_((Electric))
+       *                => http://www.pokemon.com/Pikachu_((Electric))
+       */
+      if (closing <= opening) {
+        return link_end;
+      }
+      closing--;
       link_end--;
-
-    else if (data[link_end - 1] == ';') {
+      break;
+    case '?':
+    case '!':
+    case '.':
+    case ',':
+    case ':':
+    case '*':
+    case '_':
+    case '~':
+    case '\'':
+    case '"':
+      link_end--;
+      break;
+    case ';': {
       size_t new_end = link_end - 2;
 
       while (new_end > 0 && cmark_isalpha(data[new_end]))
@@ -68,46 +101,12 @@ static size_t autolink_delim(uint8_t *data, size_t link_end) {
         link_end = new_end;
       else
         link_end--;
-    } else if (copen != 0) {
-      size_t closing = 0;
-      size_t opening = 0;
-      i = 0;
-
-      /* Allow any number of matching brackets (as recognised in copen/cclose)
-       * at the end of the URL.  If there is a greater number of closing
-       * brackets than opening ones, we remove one character from the end of
-       * the link.
-       *
-       * Examples (input text => output linked portion):
-       *
-       *	http://www.pokemon.com/Pikachu_(Electric)
-       *		=> http://www.pokemon.com/Pikachu_(Electric)
-       *
-       *	http://www.pokemon.com/Pikachu_((Electric)
-       *		=> http://www.pokemon.com/Pikachu_((Electric)
-       *
-       *	http://www.pokemon.com/Pikachu_(Electric))
-       *		=> http://www.pokemon.com/Pikachu_(Electric)
-       *
-       *	http://www.pokemon.com/Pikachu_((Electric))
-       *		=> http://www.pokemon.com/Pikachu_((Electric))
-       */
-
-      while (i < link_end) {
-        if (data[i] == copen)
-          opening++;
-        else if (data[i] == cclose)
-          closing++;
-
-        i++;
-      }
-
-      if (closing <= opening)
-        break;
-
-      link_end--;
-    } else
       break;
+    }
+
+    default:
+      return link_end;
+    }
   }
 
   return link_end;
