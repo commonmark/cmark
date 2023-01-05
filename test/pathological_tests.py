ea08b819b7d0b7ce0a0d633012dc297477b37cb9
John MacFarlane <jgm@berkeley.edu>
2023-01-04 23:11:04 -0800

    Add new pathological test for pattern "![[]()"*n.
    This was formerly quadratic.

--- test/pathological_tests.py
+++ test/pathological_tests.py
@@ -63,6 +63,9 @@ pathological = {
     "pattern [ (]( repeated":
                  (("[ (](" * 80000),
                   re.compile("(\[ \(\]\(){80000}")),
+    "pattern ![[]() repeated":
+                 ("![[]()" * 160000,
+                  re.compile("(!\[<a href=\"\"></a>){160000}")),
     "hard link/emph case":
                  ("**x [a*b**c*](d)",
                   re.compile("\\*\\*x <a href=\"d\">a<em>b\\*\\*c</em></a>")),
