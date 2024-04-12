e326ce75fc4b38de7060bc4eff33c28487b4ccce
Nick Wellnhofer <wellnhofer@aevum.de>
2024-04-12 17:10:01 +0200

    wrappers: Fix memory leak in Ruby wrapper
    Free the string returned from cmark_markdown_to_html.
    
    Fix cmark_markdown_to_html argument types.

--- wrappers/wrapper.rb
+++ wrappers/wrapper.rb
@@ -3,13 +3,25 @@ require 'ffi'
 
 module CMark
   extend FFI::Library
+
+  class Mem < FFI::Struct
+    layout :calloc, :pointer,
+           :realloc, :pointer,
+           :free, callback([:pointer], :void)
+  end
+
   ffi_lib ['libcmark', 'cmark']
-  attach_function :cmark_markdown_to_html, [:string, :int, :int], :string
+  attach_function :cmark_get_default_mem_allocator, [], :pointer
+  attach_function :cmark_markdown_to_html, [:string, :size_t, :int], :pointer
 end
 
 def markdown_to_html(s)
   len = s.bytesize
-  CMark::cmark_markdown_to_html(s, len, 0)
+  cstring = CMark::cmark_markdown_to_html(s, len, 0)
+  result = cstring.get_string(0)
+  mem = CMark::cmark_get_default_mem_allocator
+  CMark::Mem.new(mem)[:free].call(cstring)
+  result
 end
 
 STDOUT.write(markdown_to_html(ARGF.read()))
