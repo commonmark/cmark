commonmark/cmark

commit c7b28771e344ab95032c7284b3d5a011542f8ad4

build: minor tweaks for speeding up the configure phase
Only the C language is used unless we are building the ASAN or UBSAN
build variants. Only check for the C language support, enabling C++
support in the case that the build type is one of those two.

Restructure the file layout to place the custom modules into the
`cmake/modules` directory that is the common layout for CMake based
projects.

While a generally unimportant optimization, it makes it much more clear
what is being checked and why and does reduce the configure time by ~10%
(.25s) on Windows.

CMakeLists.txt
@@ -7,9 +7,14 @@ if(POLICY CMP0092)
   cmake_policy(SET CMP0092 NEW)
 endif()
 
-project(cmark VERSION 0.30.3)
+project(cmark
+  LANGUAGES C CXX
+  VERSION 0.30.3)
 
-include("FindAsan.cmake")
+if(CMAKE_BUILD_TYPE MATCHES "asan|ubsan")
+  list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake/modules)
+  include(FindAsan)
+endif()
 include(GNUInstallDirs)
 include(CTest)
 

commit 003c673c1672531b6715cedcecd5e6607cdb7f44

cmake: Fix build type checks
Commits 023d887d and c7b28771 broke the build type checks (regex is
case-sensitive).

Make the libFuzzer target use the Asan build type again.

CMakeLists.txt
@@ -11,7 +11,7 @@ project(cmark
   LANGUAGES C CXX
   VERSION 0.31.0)
 
-if(CMAKE_BUILD_TYPE MATCHES "asan|ubsan")
+if(CMAKE_BUILD_TYPE STREQUAL Asan)
   list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake/modules)
   include(FindAsan)
 endif()
@@ -85,10 +85,10 @@ function(cmark_add_compile_options target)
             -Wall -Wextra -pedantic
             $<$<COMPILE_LANGUAGE:C>:-Wstrict-prototypes>)
   endif()
-  if(CMAKE_BUILD_TYPE MATCHES profile)
+  if(CMAKE_BUILD_TYPE STREQUAL Profile)
     target_compile_options(${target} PRIVATE -pg)
   endif()
-  if(CMAKE_BUILD_TYPE MATCHES ubsan)
+  if(CMAKE_BUILD_TYPE STREQUAL Ubsan)
     target_compile_options(${target} PRIVATE -fsanitize=undefined)
   endif()
   if(CMARK_LIB_FUZZER)

Makefile
@@ -89,10 +89,12 @@ afl:
 	    $(CMARK) $(CMARK_OPTS)
 
 libFuzzer:
-	CC=clang CXX=clang++ \
-	CFLAGS=-fsanitize=address LDFLAGS=-fsanitize=address \
-	    cmake -S . -B $(BUILDDIR) \
-	        -DCMARK_LIB_FUZZER=ON
+	cmake \
+	    -S . -B $(BUILDDIR) \
+	    -DCMAKE_C_COMPILER=clang \
+	    -DCMAKE_CXX_COMPILER=clang++ \
+	    -DCMAKE_BUILD_TYPE=Asan \
+	    -DCMARK_LIB_FUZZER=ON
 	cmake --build $(BUILDDIR)
 	mkdir -p fuzz/corpus
 	$(BUILDDIR)/fuzz/cmark-fuzz \