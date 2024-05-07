2d825d0d4c513f5ee8734706ebd7a8a28bb4c129
John MacFarlane <jgm@berkeley.edu>
2024-05-07 14:01:21 -0700

    Make left/right flankingness depend on CJK characters.
    If either the character before or the character after is CJK
    the delimiter run is counted as both left- and right-flanking.
    
    The intent is to better support emphasis in languages that
    do not use spaces. See #650.

--- src/inlines.c
+++ src/inlines.c
@@ -462,13 +462,17 @@ static int scan_delims(subject *subj, unsigned char c, bool *can_open,
     after_char = 10;
   }
   left_flanking = numdelims > 0 && !cmark_utf8proc_is_space(after_char) &&
-                  (!cmark_utf8proc_is_punctuation_or_symbol(after_char) ||
+                  ((!cmark_utf8proc_is_punctuation_or_symbol(after_char) ||
                    cmark_utf8proc_is_space(before_char) ||
-                   cmark_utf8proc_is_punctuation_or_symbol(before_char));
+                    cmark_utf8proc_is_punctuation_or_symbol(before_char)) ||
+                   (cmark_utf8proc_is_CJK(before_char) ||
+                    cmark_utf8proc_is_CJK(after_char)));
   right_flanking = numdelims > 0 && !cmark_utf8proc_is_space(before_char) &&
-                   (!cmark_utf8proc_is_punctuation_or_symbol(before_char) ||
-                    cmark_utf8proc_is_space(after_char) ||
-                    cmark_utf8proc_is_punctuation_or_symbol(after_char));
+                   ((!cmark_utf8proc_is_punctuation_or_symbol(before_char)
+                     || cmark_utf8proc_is_space(after_char) ||
+                     cmark_utf8proc_is_punctuation_or_symbol(after_char)) ||
+                    (cmark_utf8proc_is_CJK(before_char) ||
+                     cmark_utf8proc_is_CJK(after_char)));
   if (c == '_') {
     *can_open = left_flanking &&
                 (!right_flanking ||
