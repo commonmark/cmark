0d9cfd8349cccffb8da4a2c9e863ced7ffff0149
John MacFarlane <jgm@berkeley.edu>
2026-04-17 09:57:45 +0200

    Add flake.nix.

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "C development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            clang
+            clang-tools  # clangd, clang-format, etc.
+            cmake
+            python3
+            re2c
+          ];
+        };
+      });
+    };
+}
